cc2a3408302ffe5dc240ac65c216c188fee9ee13
Nick <108188470+nickhallden1330@users.noreply.github.com>
2022-10-15 12:23:28 +0500

    add fragment

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 const elList = document.querySelector('.js-list');
+const elSaveList = document.querySelector('.save-list');
 const elSelect = document.querySelector('.js-select');
 const elSelec = document.querySelector('.js-selec');
 const elBtn = document.querySelector('.js-btn');
+const elModal = document.querySelector('.modal');
+
+const itemFragment = document.createDocumentFragment();
+
+let localFilms = JSON.parse(localStorage.getItem('savedFilms'));
+
+const saveList = localFilms || [];
 var theme = false;
 
 function domWiew(array, node) {
@@ -9,32 +17,65 @@ function domWiew(array, node) {
 		let elItem = document.createElement('li');
 		let elTitle = document.createElement('h3');
 		let elImg = document.createElement('img');
-		let elDiv = document.createElement('div');
-		let elText = document.createElement('p');
-		let elTime = document.createElement('time');
-		let elGender = document.createElement('p');
+		// let elDiv = document.createElement('div');
+		// let elText = document.createElement('p');
+		// let elTime = document.createElement('time');
+		// let elGender = document.createElement('p');
+		let elBtnsWrapper = document.createElement('div');
+		let elBookmarkBtn = document.createElement('button');
+		let elModalBtn = document.createElement('button');
 
 		elTitle.textContent = el.title;
-		elDiv.classList.add('wrap');
-		elText.textContent = el.overview;
+		// elDiv.classList.add('wrap');
+		// elText.textContent = el.overview;
+		elBtnsWrapper.setAttribute('class', 'btns-wrapper');
+		elBookmarkBtn.textContent = 'Save';
+		elBookmarkBtn.setAttribute('class', 'save-btn');
+		elBookmarkBtn.dataset.filmId = el.id;
+		elModalBtn.textContent = 'Info';
+		elModalBtn.setAttribute('class', 'info-btn');
 		elImg.src = el.poster;
 		elImg.setAttribute('width', '250');
 		elImg.setAttribute('height', '350');
 		elImg.setAttribute('alt', 'Films');
-		elTime.textContent = year(el.release_date);
-		elGender.textContent = el.genres;
+		// elTime.textContent = year(el.release_date);
+		// elGender.textContent = el.genres;
 
 		elItem.appendChild(elTitle);
+		elItem.appendChild(elBtnsWrapper);
+		elBtnsWrapper.appendChild(elBookmarkBtn);
+		elBtnsWrapper.appendChild(elModalBtn);
 		elItem.appendChild(elImg);
-		elDiv.appendChild(elText);
-		elItem.appendChild(elDiv);
-		elItem.appendChild(elTime);
-		elItem.appendChild(elGender);
-		node.appendChild(elItem);
+		// elDiv.appendChild(elText);
+		// elItem.appendChild(elDiv);
+		// elItem.appendChild(elTime);
+		// elItem.appendChild(elGender);
+		itemFragment.appendChild(elItem);
 	});
+	node.appendChild(itemFragment)
 }
 
 domWiew(films, elList);
+
+const renderSave = (array, node) => {
+	localStorage.setItem('savedFilms', JSON.stringify(array));
+	node.innerHTML = '';
+	array.forEach((el) => {
+		const newItem = document.createElement('li');
+		const newBtn = document.createElement('button');
+
+		newItem.textContent = el.title;
+		newBtn.textContent = 'X';
+
+		newBtn.setAttribute('class', 'btn-delete');
+		newBtn.dataset.filmId = el.id;
+
+		newItem.appendChild(newBtn);
+		node.appendChild(newItem);
+	});
+};
+
+renderSave(saveList, elSaveList);
 
 function year(format) {
 	var date = new Date(format);
@@ -122,3 +163,29 @@ function changeThem() {
 	}
 }
 changeThem();
+
+elList.addEventListener('click', (evt) => {
+	if (evt.target.matches('.save-btn')) {
+		const clickedId = evt.target.dataset.filmId;
+
+		const findedItem = films.find((el) => el.id == clickedId);
+
+		if (!saveList.includes(findedItem)) {
+			saveList.push(findedItem);
+		}
+		renderSave(saveList, elSaveList);
+	}
+});
+
+elSaveList.addEventListener('click', (evt) => {
+	if (evt.target.matches('.btn-delete')) {
+		const deletedItemId = evt.target.dataset.filmId;
+
+		const deletedItem = saveList.findIndex((el) => el.id == deletedItemId);
+
+		saveList.splice(deletedItem, 1);
+		renderSave(saveList, elSaveList);
+	}
+	if (evt.target.matches('.info-btn')) {
+	}
+});
